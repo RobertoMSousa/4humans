5edba8c18c17b4cbd66290b138832eb2205c73ac
Roberto Sousa <beto.sousa22@gmail.com>
2018-05-31 17:55:32 +0100

    add the basic gatsby files

---
+++ gatsby-config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: ['gatsby-plugin-react-helmet',
+    `gatsby-plugin-catch-links`, {
+    resolve: `gatsby-plugin-typography`,
+    options: {
+      pathToConfigModule: `src/utils/typography.js`,
+    },
+  },
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-typescript`,
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `src`,
+      path: `${__dirname}/src/content`,
+    },
+  },
+  {
+    resolve: 'gatsby-transformer-remark',
+    options: {
+      plugins: []
+    }
+  },
+  {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
+      // Puts tracking script in the head instead of the body
+      head: false,
+      // Setting this parameter is optional
+      anonymize: true,
+      // Setting this parameter is also optional
+      respectDNT: true
+    }
+  }],
+}

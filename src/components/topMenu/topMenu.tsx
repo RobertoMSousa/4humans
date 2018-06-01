fb471b9b3171a3558b9481af93cf6b43c599565f
Roberto Sousa <beto.sousa22@gmail.com>
2018-06-01 08:58:52 +0100

    add navigation to top menu

--- src/components/topMenu/topMenu.tsx
+++ src/components/topMenu/topMenu.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
-import Link from "gatsby-link";
-import { GraphQLID } from "graphql";
+// import Link from "gatsby-link";
+// import { connect } from "react-redux";
+// import { Dispatch } from "redux";
+// import * as PropTypes from "prop-types";
 
 // css
 import "./topMenu.css";
@@ -11,22 +13,39 @@ const blogSvg = require("./blog.svg");
 const courseSvg = require("./courses.svg");
 const booksSvg = require("./books.svg");
 
+import { navigateTo } from "gatsby-link"
+
 const TopMenu = () => (
 	<div className="topMenuWrapper">
-		<div className="topMenuHome center">
-			<img src={homeSvg} className="" alt=""/>
+		<div
+			className="topMenuHome center"
+			onClick={ () => navigateTo("/")}
+		>
+			<img src={homeSvg} className="" alt="" />
 			<span>Home</span>
 		</div>
-		<div className="topMenuBooks center">
-			<img src={booksSvg} className="" alt=""/>
+
+		<div
+			className="topMenuBooks center"
+			onClick={ () => navigateTo("/books")}
+		>
+			<img src={booksSvg} className="" alt="" />
 			<span>Books</span>
 		</div>
-		<div className="topMenuCourses center">
-			<img src={courseSvg} className="" alt=""/>
+
+		<div
+			className="topMenuCourses center"
+			onClick={ () => navigateTo("/courses")}
+		>
+			<img src={courseSvg} className="" alt="" />
 			<span>Courses</span>
 		</div>
-		<div className="topMenuBlog center">
-			<img src={blogSvg} className="topMenuIcon" alt=""/>
+
+		<div
+			className="topMenuBlog center"
+			onClick={ () => navigateTo("/blog")}
+		>
+			<img src={blogSvg} className="topMenuIcon" alt="" />
 			<span className="topMenuText">Blog</span>
 		</div>
 	</div>

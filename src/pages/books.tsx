c5998ab3dc1f8c2b79c9e09357524c755166b021
Roberto Sousa <beto.sousa22@gmail.com>
2018-06-01 19:31:18 +0100

    create books and courses starter page

---
+++ src/pages/books.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const BooksPage = () => (
+	<div>
+		<h1>BooksPage</h1>
+	</div>
+);
+
+export default BooksPage;
---
+++ src/pages/courses.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const CoursesPage = () => (
+	<div>
+		<h1>CoursesPage</h1>
+	</div>
+);
+
+export default CoursesPage;

85a5a433a97fcdeefd1182fff3c2bda8f0db8452
Roberto Sousa <beto.sousa22@gmail.com>
2018-06-02 18:28:18 +0100

    start the index page style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,29 @@
 import * as React from "react";
 import Link from "gatsby-link";
 
+// css
+import "./css/index.scss";
+
 export default () => (
-	<div>
-		<h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
-		<p>
-			What do I like to do? Lots of course but definitely enjoy building
-			websites.
-		</p>
+	<div className="indexPageWrapper">
+		<div className="indexWhy"></div>
+		<div className="indexHow"></div>
+		<div className="indexWhat"></div>
+		<div className="indexFooter">
+			<div className="indexFooterAbout">
+				<span className="indexFooterTitle center">About us</span>
+				<div className="indexFooterAboutBottom"></div>
+			</div>
+			<div className="indexFooterSupport">
+				<span className="indexFooterTitle center">Support this dream</span>
+				<div className="indexFooterSupportButton center">
+					<span className="center">Support on Patreon</span>
+				</div>
+			</div>
+			<div className="indexFooterNewsletter">
+				<span className="indexFooterTitle center">Newsletter</span>
+				<div className="indexFooterNewsletterBottom"></div>
+			</div>
+		</div>
 	</div>
 );

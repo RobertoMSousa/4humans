79c630076b28f8b89e15def42b7123e9367b7300
Roberto Sousa <beto.sousa22@gmail.com>
2018-06-03 01:55:06 +0100

    add no select option to all the app

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -5,7 +5,7 @@ import Header from "../components/header/header";
 import "./index.scss";
 
 const Layout: any = ({ children, data }) => (
-	<div style={{height: "100vh"} } >
+	<div style={{height: "100vh"} } className="noSelect">
 		<Helmet
 			title={data.site.siteMetadata.title}
 			meta={[

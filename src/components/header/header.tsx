fc1f35159da798021e5b7d0f817990271d783ceb
Roberto Sousa <beto.sousa22@gmail.com>
2018-06-03 15:54:50 +0100

    use the map on the header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -19,7 +19,7 @@ const Header = ({ siteTitle }) => (
 							enable: true,
 							value_area: 868.0624057955,
 						},
-						value: 80,
+						value: 100,
 					},
 					color: {
 						value: "#ecd018",
@@ -31,88 +31,49 @@ const Header = ({ siteTitle }) => (
 							width: 0,
 						},
 						polygon: {
-							nb_sides: 5,
+							nb_sides: 3,
 						},
 					},
 					opacity: {
 						value: 0.5,
 						random: true,
-						anim: {
-							enable: false,
-							speed: 1,
-							opacity_min: 0.1,
-							sync: false,
-						},
+						// anim: {
+						// 	enable: false,
+						// 	speed: 1,
+						// 	opacity_min: 0.1,
+						// 	sync: false,
+						// },
 					},
 					size: {
 						value: 3,
 						random: true,
-						anim: {
-							enable: false,
-							speed: 40,
-							size_min: 0.1,
-							sync: false,
-						},
+						// anim: {
+						// 	enable: false,
+						// 	speed: 40,
+						// 	size_min: 0.1,
+						// 	sync: false,
+						// },
 					},
 					line_linked: {
 						enable: true,
 						distance: 150,
 						color: "#e2d018",
-						opacity: 0.4,
+						opacity: 0.6,
 						width: 1,
 					},
 					move: {
 						enable: true,
 						speed: 5,
 						direction: "none",
-						random: false,
+						random: true,
 						straight: false,
 						out_mode: "out",
 						bounce: false,
-						attract: {
-							enable: false,
-							rotateX: 600,
-							rotateY: 1200,
-						},
-					},
-				},
-				interactivity: {
-					detect_on: "canvas",
-					events: {
-						onhover: {
-							enable: false,
-							mode: "grab",
-						},
-						onclick: {
-							enable: false,
-							mode: "push",
-						},
-						resize: false,
-					},
-					modes: {
-						grab: {
-							distance: 400,
-							line_linked: {
-								opacity: 1,
-							},
-						},
-						bubble: {
-							distance: 400,
-							size: 40,
-							duration: 2,
-							opacity: 8,
-							speed: 3,
-						},
-						repulse: {
-							distance: 200,
-							duration: 0.4,
-						},
-						push: {
-							particles_nb: 4,
-						},
-						remove: {
-							particles_nb: 2,
-						},
+						// attract: {
+						// 	enable: false,
+						// 	rotateX: 600,
+						// 	rotateY: 1200,
+						// },
 					},
 				},
 				retina_detect: true,

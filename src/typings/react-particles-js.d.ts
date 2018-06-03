b6fba97e36836cbce9b08d1aea8ccdf41fac90aa
Roberto Sousa <beto.sousa22@gmail.com>
2018-06-03 11:10:19 +0100

    turn the height of the particles optional

--- src/typings/react-particles-js.d.ts
+++ src/typings/react-particles-js.d.ts
@@ -4,15 +4,10 @@ declare module "react-particles-js" {
     import * as React from 'react'
     export interface ParticlesProps {
         width: string,
-        height: string,
+        height?: string,
         params: any,
         style: any
     }
     const Particles: React.ComponentClass<ParticlesProps>
     export default Particles
 }
-
-
-// declare const graphql: (query: TemplateStringsArray) => void;
-
-// declare const ParticlesProps: (width: string, height: string, params: any, style: any) => void;

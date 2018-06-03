76d9bebcfa5d66f2287fe73b84f935b52af79618
Roberto Sousa <beto.sousa22@gmail.com>
2018-06-04 00:54:27 +0100

    remove dead code

--- src/typings/declarations.d.ts
+++ src/typings/declarations.d.ts
@@ -1,6 +1,4 @@
 declare const graphql: (query: TemplateStringsArray) => void;
-
-// declare const ParticlesProps: (width: string, height: string, params: any, style: any) => void;
   
 declare module "disqus-react" {
   export class DiscussionEmbed extends React.Component<{

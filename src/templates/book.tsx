aaa04978d226e0ab802df6e500673f377f4bb2ec
Roberto Sousa <beto.sousa22@gmail.com>
2018-06-05 02:51:13 +0100

    create the book template

---
+++ src/templates/book.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import Helmet from "react-helmet";
+
+export default function Template({
+	data,
+}) {
+	const { markdownRemark: post } = data;
+	return (
+		<div className="blog-post-container">
+			<Helmet title={`Your book  name- ${post.frontmatter.title}`} />
+			<div className="blog-post">
+				<h1>
+					Super book name -> {post.frontmatter.title}
+				</h1>
+				<div
+					className="blog-post-content"
+					dangerouslySetInnerHTML={{ __html: post.html }}
+				/>
+			</div>
+		</div>
+	);
+}
+
+export const pageQuery = graphql`
+	query BookByPath($path: String!) {
+		markdownRemark(frontmatter: { path: { eq: $path } }) {
+			html
+			frontmatter {
+				date(formatString: "MMMM DD, YYYY")
+				path
+				title
+			}
+		}
+	}
+`;

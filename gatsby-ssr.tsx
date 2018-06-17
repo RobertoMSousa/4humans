bed6d3b36388778e9c351d14f258bf9ab98b37ca
Roberto Sousa <beto.sousa22@gmail.com>
2018-06-17 15:34:16 +0100

    couple lint rules fixed

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { renderToString } from "react-dom/server";
 
 exports.replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
-	const ConnectedBody = () => (
+	const ConnectedBody: any = () => (
 		{bodyComponent}
 	);
 	replaceBodyHTMLString(renderToString(<ConnectedBody />));
